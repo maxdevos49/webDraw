ade5a979fc8f988241b3b77b4d33e457659e1981
Maxwell DeVos <maxdevos49@gmail.com>
2018-02-28 20:07:06 -0600

    hgjgjghjhg

--- public/JS/CanvasClass.js
+++ public/JS/CanvasClass.js
@@ -37,10 +37,10 @@ class Canvas {
 	canvasBackground(){//change the background of the canvas between white and transparent background
 
 	    if(this.grid){
-	        document.getElementById(`${this.id}Window`).style.backgroundImage = "none";
-	        document.getElementById(`${this.id}Window`).style.backgroundColor = "white";
+	        document.getElementById(`${this.id}`).style.backgroundImage = "none";
+	        document.getElementById(`${this.id}`).style.backgroundColor = "white";
 	    }else{
-	        document.getElementById(`${this.id}Window`).style.backgroundImage = "linear-gradient(45deg, #d4d6d8 25%, transparent 10%), linear-gradient(-45deg, #d4d6d8 25%, transparent 10%), linear-gradient(45deg, transparent 75%, #d4d6d8 75%), linear-gradient(-45deg, transparent 75%, #d4d6d8 75%)";
+	        document.getElementById(`${this.id}`).style.backgroundImage = "linear-gradient(45deg, #d4d6d8 25%, transparent 10%), linear-gradient(-45deg, #d4d6d8 25%, transparent 10%), linear-gradient(45deg, transparent 75%, #d4d6d8 75%), linear-gradient(-45deg, transparent 75%, #d4d6d8 75%)";
 	    }
 
 	    this.grid = !this.grid;
